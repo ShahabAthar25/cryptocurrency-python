aa7329f5375cdffd845eaf43c18e0d26f0a0ff2c
ShahabAthar25 <shahabathar25@gmail.com>
2021-10-30 13:40:18 +0500

    Add Route

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify
 
 from SECRET_KEY import key
 from database import db_init
@@ -16,7 +16,7 @@ db_init(app)
 
 @app.route('/', methods=["GET"]) # Setting Route
 def index():
-    return 'Hello World' # Returning msg
+    return 'hello world' # Returning msg
 
 if __name__ == "__main__": # checking if the script is being executed or called
     app.run(debug=True) # running the app in debug mode
